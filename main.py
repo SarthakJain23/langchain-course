703950f88e518ec37090f408c35015cb1c1d5c58
Sarthak Jain <sarthak.jain@turom.co>
2026-07-21 22:23:38 +0530

    feat: define AgentResponse schema and integrate into create_agent configuration

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ from langchain.messages import HumanMessage
 from langchain.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
 
 tavily_search = TavilySearch()
+
+
+class Source(BaseModel):
+    """Schema for the source used by the agent"""
+
+    url: str = Field(description="The url of the source")
+
+
+class AgentResponse(BaseModel):
+    """Schema for the response used by the agent"""
+
+    answer: str = Field(description="The answer to the query")
+    sources: list[Source] = Field(
+        default_factory=list, description="List of sources used by the agent to generate the answer"
+    )
 
 
 @tool
@@ -25,7 +41,7 @@ def get_current_date() -> str:
 
 llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
 tools = [tavily_search, get_current_date]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
